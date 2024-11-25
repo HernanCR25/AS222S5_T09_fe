ea78ec63f459710b130242426dd90018cf5c5578
Hernán Chumpitaz <111783000+HernanCR25@users.noreply.github.com>
2024-11-25 17:17:36 -0500

    Update environment.prod.ts

--- src/enviroment/environment.prod.ts
+++ src/enviroment/environment.prod.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   //apigpt_fe: 'https://expert-sniffle-x74wp754r4vfw66-8085.app.github.dev/api'
   apigpt_fe: '${API_BACKEND}'
 };

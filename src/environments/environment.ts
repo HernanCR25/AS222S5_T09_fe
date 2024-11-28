cc66d44e8d2df403d2fff05cc9b3509b8b64e1b6
Hernán Chumpitaz <111783000+HernanCR25@users.noreply.github.com>
2024-11-28 03:16:38 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  //apigpt_fe: 'https://expert-sniffle-x74wp754r4vfw66-8085.app.github.dev/api'
+  //apigpt_fe: 'https://potential-meme-gjpqrj4ppvr3p4q7-8085.app.github.dev/api'
   apigpt_fe: '${API_BACKEND}'
 };
 
